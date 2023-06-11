ab9f194579fb55bd6a73f06e51aa51c87cd2cec8
Alwin <alwin>
2023-06-11 16:16:31 +0530

    add coming soon page

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    return render_template('coming_soon.html')
+    print(html_code)  # Print the HTML code in the console
+@app.route('/new')
+def new():
+    return  render_template('new.html')
+
+
+if __name__ == '__main__':
+    app.run(port=7777)  # Change the port number to 7777 or any available port
