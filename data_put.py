6d0742e78b77519574e0f0f06bfcf0ec79996dba
Alwin <alwin>
2023-06-24 12:04:40 +0530

    change path in data_put.py

--- data_put.py
+++ data_put.py
@@ -1,7 +1,15 @@
 import sqlite3
-
+from os import path
 def insert_post(title, content, author, published_date, reading_time, thumbnail_image, detailed_article):
-    conn = sqlite3.connect('articles.db')
+    # Get the directory of the current script
+    current_dir = path.dirname(path.abspath(__file__))
+
+    # Specify the filename of the database
+    db_filename = 'articles.db'
+
+    # Construct the full path to the database
+    db_path = path.join(current_dir, db_filename)
+    conn = sqlite3.connect(db_path)
     cursor = conn.cursor()
 
     cursor.execute(
